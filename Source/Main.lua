a6ee7db791aaee4286d08545dd5bb237f9edd008
Selivus08 <155065631+Selivus08@users.noreply.github.com>
2025-06-13 06:57:06 -0500

    Update Main.lua

--- Source/Main.lua
+++ Source/Main.lua
@@ -1,4 +1,4 @@
-local pathToGithub = "https://raw.githubusercontent.com/xhayper/Animator/main/Source/"
+local pathToGithub = "https://raw.githubusercontent.com/selivus08/Animator/main/Source/"
 
 local sub = string.sub
 
